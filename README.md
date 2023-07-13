18b371ac6d290aaf3f3b5dca5a7b5d6bad543be9
RodrigoGonzalez78 <61030017+RodrigoGonzalez78@users.noreply.github.com>
2023-07-13 18:04:28 -0300

    rutas

---
+++ README.md
@@ -0,0 +1,5 @@
+#Rest api para gestionar tareas
+
+Permite gestionar usuario y tareas para cada usuario.
+
+Con gorilla mux y gorm, utilsando como base de datos posgret sql
--- main.go
+++ main.go
@@ -1,5 +1,38 @@
 package main
 
+import (
+	"net/http"
+
+	"github.com/RodrigoGonzalez78/tasks_management_backend/db"
+	"github.com/RodrigoGonzalez78/tasks_management_backend/models"
+	"github.com/RodrigoGonzalez78/tasks_management_backend/routes"
+	"github.com/gorilla/mux"
+)
+
 func main() {
 
+	db.DBConnection()
+
+	//Se crean las tablas users y task
+	db.DB.AutoMigrate(models.User{})
+	db.DB.AutoMigrate(models.Task{})
+
+	router := mux.NewRouter()
+
+	//Ruta home
+	router.HandleFunc("/", routes.HomeHandler)
+
+	//Ruta crud de usuarios
+	router.HandleFunc("/users", routes.GetUsersHandler).Methods("GET")
+	router.HandleFunc("/users/{id}", routes.GetUserHandler).Methods("GET")
+	router.HandleFunc("/users", routes.PostUserHandler).Methods("POST")
+	router.HandleFunc("/users/{id}", routes.DeleteUserHandler).Methods("DELETE")
+
+	//Rutas del crud de tareas
+	router.HandleFunc("/tasks", routes.GetTaksHandler).Methods("GET")
+	router.HandleFunc("/tasks/{id}", routes.GetTaskHandler).Methods("GET")
+	router.HandleFunc("/tasks", routes.PostTaskHandler).Methods("POST")
+	router.HandleFunc("/tasks/{id}", routes.DeleteTaskHandler).Methods("DELETE")
+	//Iniciamos el servidor
+	http.ListenAndServe(":3000", router)
 }
