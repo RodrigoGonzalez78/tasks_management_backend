798576c20d5938514d683509b75d943b06121f79
RodrigoGonzalez78 <61030017+RodrigoGonzalez78@users.noreply.github.com>
2023-11-05 02:44:28 -0300

    correcion

--- go.mod
+++ go.mod
@@ -1,4 +1,4 @@
-module github.com/RodrigoGonzalez78/tasks
+module github.com/RodrigoGonzalez78/tasks_management_backend
 
 go 1.20
 
--- main.go
+++ main.go
@@ -3,9 +3,9 @@ package main
 import (
 	"net/http"
 
-	"github.com/RodrigoGonzalez78/tasks/db"
-	"github.com/RodrigoGonzalez78/tasks/models"
-	"github.com/RodrigoGonzalez78/tasks/routes"
+	"github.com/RodrigoGonzalez78/tasks_management_backend/db"
+	"github.com/RodrigoGonzalez78/tasks_management_backend/models"
+	"github.com/RodrigoGonzalez78/tasks_management_backend/routes"
 	"github.com/gorilla/mux"
 )
 
@@ -13,7 +13,8 @@ func main() {
 
 	db.DBConnection()
 
-	//Se crean las tablas task
+	//Se crean las tablas users y task
+	db.DB.AutoMigrate(models.User{})
 	db.DB.AutoMigrate(models.Task{})
 
 	router := mux.NewRouter()
@@ -21,13 +22,17 @@ func main() {
 	//Ruta home
 	router.HandleFunc("/", routes.HomeHandler)
 
+	//Ruta crud de usuarios
+	router.HandleFunc("/users", routes.GetUsersHandler).Methods("GET")
+	router.HandleFunc("/users/{id}", routes.GetUserHandler).Methods("GET")
+	router.HandleFunc("/users", routes.PostUserHandler).Methods("POST")
+	router.HandleFunc("/users/{id}", routes.DeleteUserHandler).Methods("DELETE")
+
 	//Rutas del crud de tareas
 	router.HandleFunc("/tasks", routes.GetTaksHandler).Methods("GET")
 	router.HandleFunc("/tasks/{id}", routes.GetTaskHandler).Methods("GET")
 	router.HandleFunc("/tasks", routes.PostTaskHandler).Methods("POST")
 	router.HandleFunc("/tasks/{id}", routes.DeleteTaskHandler).Methods("DELETE")
-	router.HandleFunc("/tasks/user/{userId}", routes.GetTasksByUserHandler).Methods("GET")
-
 	//Iniciamos el servidor
-	http.ListenAndServe(":4000", router)
+	http.ListenAndServe(":3000", router)
 }
